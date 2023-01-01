7ca0dc46d8b7fa773ec3f026b47c818846c60c58
Harimad <leesh68481@gmail.com>
2023-01-01 22:40:35 +0900

    feat: Link 작업

--- pages/projects.js
+++ pages/projects.js
@@ -1,8 +1,15 @@
 import Layout from "../components/layout";
+import Head from "next/head";
 
 export default function Projects() {
   return (
     <Layout>
+      <Head>
+        <title>Tim`s portfolio</title>
+        <meta name="description" content="Tim의 포트폴리오" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <h1>프로젝트</h1>
     </Layout>
   );
