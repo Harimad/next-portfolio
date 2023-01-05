db4d237e1c16d74f17df712a2077a0c582f14640
Harimad <leesh68481@gmail.com>
2023-01-05 22:52:28 +0900

    typewriter-effect 모듈 적용

--- components/home/hero.js
+++ components/home/hero.js
@@ -1,15 +1,34 @@
 import Animation from "./animation";
 import Link from "next/link";
+import Typewriter from "typewriter-effect";
 
 export default function Home() {
   return (
     <>
       <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-          안녕하세요!
+        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-bold text-gray-900">
+          {/* 안녕하세요! */}
           <br className="hidden lg:inline-block" />
-          기록하는 프론트엔드 개발자 이성현 입니다🤗
+          {/* 기록하는 프론트엔드 개발자 이성현 입니다🤗 */}
+          <Typewriter
+            options={{
+              strings: ["안녕하세요!"],
+              autoStart: true,
+              loop: true,
+              pauseFor: 3000,
+            }}
+          />
+          <div className="mt-3 font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+            <Typewriter
+              options={{
+                strings: ["기록하는 프론트엔드 개발자", "이성현 입니다 :)"],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </div>
         </h1>
+
         <p className="leading-relaxed">
           유지 보수에 좋은 코드를 고민하지만, 사용자와 서비스를 먼저 생각하는
           개발자를 지향합니다.
